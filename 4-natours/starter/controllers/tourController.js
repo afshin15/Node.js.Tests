07936ac44a9edbd98c035ceef7042acd37f74dbc
afshin15 <afshin74@gmail.com>
2023-04-13 09:46:49 -0600

    End of Section 8

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,8 +1,18 @@
-const fs = require('fs');
+//const fs = require('fs');
+const Tour = require(`./../models/tourModel`);
+const APIFeatures = require('./../utils/apiFeatures');
 
-const tours = JSON.parse(
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+/*const tours = JSON.parse(
   fs.readFileSync(`${__dirname}\\..\\dev-data\\data\\tours-simple.json`)
 );
+*/
 
 /* const tours = JSON.parse(
       fs.readFileSync(
@@ -11,7 +21,7 @@ const tours = JSON.parse(
     ); */
 
 //for Param middleware
-exports.checkID = (req, res, next, val) => {
+/* exports.checkID = (req, res, next, val) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -19,9 +29,9 @@ exports.checkID = (req, res, next, val) => {
     });
   }
   next();
-};
+}; */
 
-exports.checkBody = (req, res, next) => {
+/* exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
@@ -29,48 +39,121 @@ exports.checkBody = (req, res, next) => {
     });
   }
   next();
+}; */
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requetsedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
+/* exports.getAllTours = async (req, res) => {
+  try {
+    /*Two types of filtering:
+    1)Mango
+     const tours = await  Tour.find({
+      duration: 5,
+      difficulty:'easy'
+    });
+    
+    2) Mangoose:
+      const tours = await Tour.find().Where('duartion').equals(5).where('difficulty').equals('easy')
+    
 
-  const id = req.params.id * 1;
+    // console.log(req.requestTime);
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      //requetsedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+}; */
 
-  const tour = tours.find((el) => el.id === id);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id); //console.log(req.params);
 
-  /*  if (!tour) {
+    //const id = req.params.id * 1;
+
+    //const tour = tours.find((el) => el.id === id);
+
+    /*  if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   } */
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  console.log(req.body);
+exports.createTour = async (req, res) => {
+  try {
+    //const newTour = await new Tour(req.body);
+    //newTour.save();
+    //console.log(req);
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+
+  /*console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
+  tours.push(newTour);*/
+  /*  fs.writeFile(
     `${__dirname}\\dev-data\\data\\tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
@@ -81,35 +164,153 @@ exports.createTour = (req, res) => {
         },
       });
     }
-  );
+  );*/
 };
 
-exports.updateTour = (req, res) => {
-  /*  if (req.params.id * 1 > tours.length) {
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidator: true,
+    });
+
+    /*  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   } */
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  /*  if (req.params.id * 1 > tours.length) {
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    /*  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   } */
 
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+//Aggregation pipeline
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          num: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRation: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      /*{
+        $match: { _id: { $ne: 'EASY' } },
+      },*/
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numToursStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numToursStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 //define environment variables configuration
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('FB Connectionm successfull');
+  });
 
 const app = require('./app');
 
